7dcf8dd1e0ecbdd19658b003d5ab6a7a70494fb6
coderou <13739477489@qq.com>
2021-03-01 14:38:54 +0800

    代理服务器配置proxy

---
+++ src.1/Views/Search/index.vue
@@ -0,0 +1,42 @@
+<template>
+  <section class="jumbotron">
+    <h3 class="jumbotron-heading">Search Github Users</h3>
+    <div>
+      <input
+        type="text"
+        v-model="searchName"
+        placeholder="enter the name you search"
+      />
+      <button @click="sendSearchName">Search</button>
+    </div>
+  </section>
+</template>
+<script type="text/ecmascript-6">
+export default {
+  name: "Search",
+  data() {
+    return {
+      searchName: "",
+      oldsSearchName: "",
+    };
+  },
+  methods: {
+    sendSearchName(searchName) {
+      const serchName = this.searchName.trim(); //去除空格
+      if (!searchName) {
+        //如果用户没有输入内容,不发送请求
+        alert("请输入搜索内容");
+        return;
+      }
+      if (this.oldsSearchName === searchName) {
+        return;
+      }
+      this.$bus.$emit("sendSearchName", serchName);
+      this.oldsSearchName = searchName;
+    },
+  },
+};
+</script>
+
+<style scoped>
+</style>
---
+++ src_3.2_1.代理服务器配置/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="container">
+    <Search />
+    <List />
+  </div>
+</template>
+<script type="text/ecmascript-6">
+import Search from "./Views/Search";
+import List from "./Views/List";
+export default {
+  name: "App",
+  components: {
+    Search,
+    List,
+  },
+};
+</script>
+
+<style scoped>
+</style>
